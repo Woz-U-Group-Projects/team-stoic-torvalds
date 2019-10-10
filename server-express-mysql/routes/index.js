57581caaeb27da5a03a44a461aa0ffa1bc1d32f1
Sean <scullsean@gmail.com>
2019-10-10 13:17:03 -0700

    Figuring out how to build and migrate connections between mysql work bench and our project

--- server-express-mysql/routes/index.js
+++ server-express-mysql/routes/index.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var router = express.Router();
 const mysql = require('mysql');
+var mysql = require('mysql2');
+
 
 var connection = mysql.createConnection({
   host: 'localhost',
