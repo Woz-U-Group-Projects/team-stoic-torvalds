e3a8e3a340cdb8f39dfcc58298922237fa1e415c
clifford ephriam <kubby9286@gmail.com>
2019-10-20 19:18:21 -0700

    added message components  CRUD operations

--- client-angular/src/app/app.module.ts
+++ client-angular/src/app/app.module.ts
@@ -9,10 +9,13 @@ import { MatSliderModule } from '@angular/material/slider';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 import { DialogBoxComponent } from './components/dialog-box/dialog-box.component';
+import { MessageComponent } from './message/message.component';
+import { MessageAddComponent } from './message-add/message-add.component';
+import { MessageEditComponent } from './message-edit/message-edit.component';
 
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent, DialogBoxComponent],
+  declarations: [AppComponent, NavBarComponent, DialogBoxComponent, MessageComponent, MessageAddComponent, MessageEditComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule, BrowserAnimationsModule, MatSliderModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client-angular/src/app/message-add/message-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-message-add',
+  templateUrl: './message-add.component.html',
+  styleUrls: ['./message-add.component.css']
+})
+export class MessageAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
